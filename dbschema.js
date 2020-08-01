0825e0c91567d20e01cb2888da3d6814af12504d
unknown <moh.syarif93@gmail.com>
2020-08-01 08:21:52 +0800

    functions code

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-07-30T04:50:36.349Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports =  {
+    apiKey: "AIzaSyBayRlQpX4HWH_oSWbOszzzLZFLJ0_Mk2E",
+    authDomain: "sosialape-a730d.firebaseapp.com",
+    databaseURL: "https://sosialape-a730d.firebaseio.com",
+    projectId: "sosialape-a730d",
+    storageBucket: "sosialape-a730d.appspot.com",
+    messagingSenderId: "447224860283",
+    appId: "1:447224860283:web:55ff910a60ae3a985e5cde",
+    measurementId: "G-4E7K8BDHST"
+};
